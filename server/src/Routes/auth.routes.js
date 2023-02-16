c2e6ac5b500ecb5fbc9b65e56a4a7e619316c72f
Shoaib Mehmood <shoaib.ultracodes@gmail.com>
2023-02-16 17:12:04 +0500

    Login functionality

--- server/src/Routes/auth.routes.js
+++ server/src/Routes/auth.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const authController = require("../Controllers/auth.controller");
 
 router.route('/sign-up').post(authController.signup);
+router.route('/login').post(authController.login);
 
 module.exports = router;
